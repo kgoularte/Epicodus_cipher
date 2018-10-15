2136cc79955604c194944a0ec6c46cf79978fe8c
kg and ss <kg@>
2018-10-15 16:43:29 -0700

    add new function to reverse the retrieved first and last letter

--- js/scripts.js
+++ js/scripts.js
@@ -7,16 +7,31 @@ var capitalize1andlast=function(x)
   var a = enteredsentence.length;
   return a;
 };
+
 var b=capitalize1andlast(enteredsentence);
-//alert(b);
-
+alert(b);
 firstchar=enteredsentence.charAt(0);
-//alert(firstchar);
+alert(firstchar);
 lastchar=enteredsentence.charAt(b-1);
+var newstr=firstchar.concat(lastchar);
+var newstring=(newstr.toUpperCase());
+alert(newstring);
+console.log(newstring);
 
-var newstring=firstchar.concat(lastchar);
-//alert(newstring);
 
-alert(newstring.toUpperCase());
+
+
+var reverse=function(x)
+{
+  temp=newstring.split('');
+//fn1op=newstring.reverse();
+temp1=temp.reverse();
+fn1op=temp1.join('');
+
+return fn1op;
+};
+
+var newstring1=reverse(newstring);
+alert(newstring1);
 
 });
