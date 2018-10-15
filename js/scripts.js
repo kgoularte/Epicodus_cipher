e7e14fd7cfe32fdce2c085531e2f6ea24fb2c291
kg and ss <kg@>
2018-10-15 16:09:50 -0700

    pull the first and last letters and convert it to uppercase

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
   var enteredsentence=prompt("Enter a sentence");
   console.log(enteredsentence);
+
+var capitalize1andlast=function(x)
+{
+  var a = enteredsentence.length;
+  return a;
+};
+var b=capitalize1andlast(enteredsentence);
+//alert(b);
+
+firstchar=enteredsentence.charAt(0);
+//alert(firstchar);
+lastchar=enteredsentence.charAt(b-1);
+
+var newstring=firstchar.concat(lastchar);
+//alert(newstring);
+
+alert(newstring.toUpperCase());
+
 });
